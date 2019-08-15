a1124fa432e3c1a70bf550ee4228c415fa6de9c3
Justin Leach <leachjustin18@yahoo.com>
2019-08-15 11:08:54 -0500

    Add InputField component

---
+++ src/components/form/InputField.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const InputField: FC = () => {
+  return (
+    <View>
+      <Text>Input Field</Text>
+    </View>
+  );
+};
+
+export default InputField;
